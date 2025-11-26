9db09c36f181024be0ed7f75ffbb3c76e6dc5810
anikettikkal <tikkalaniket2019@gmail.com>
2025-11-26 11:26:28 +0530

    setup for backend completed

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("app is ready")
+})
+
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen('port',()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
