d6731cb8e8003b6bbff2e63d3f982f079f75496f
anikettikkal <tikkalaniket2019@gmail.com>
2025-11-27 09:34:41 +0530

    cors and cookie parser install and server is being started

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 import coonectDB from "./db/index.js";
 import dotenv from "dotenv";
+import app from "./app.js";
 
 dotenv.config({
     path: './.env'
 })
 
-coonectDB();
+coonectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`SERVER IS RUNNING AT PORT ${process.env.PORT || 8000}`);
+    })
+})
+.catch((err)=>{
+    console.log("DB CONNECTION FAILED !!!",err);
+})
 
 
 
